def784968fef3496a9264b3f7c4a4d6bff216d86
Randi Iskandar <randi.iskandar@gmail.com>
2017-10-12 21:42:44 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -26,8 +26,7 @@ if(x=="1"){xxx="/2017/10/fresh-car-versus-old-car-which.html"}
 if(x=="2"){xxx="/2017/10/ways-to-get-approved-car-financing-in.html"}
 if(x=="3"){xxx="/2017/10/top-features-of-different-female-credit.html"}
 if(x=="4"){xxx="/2017/10/appear-beyond-medications-to-treat.html"}
-if(x=="5"){xxx="/2017/10/bv-remedy-find-most-efficient-remedies.html"}
-if(x=="6"){xxx="/2017/10/natural-aloe-vera-hydrating-cream-to.html"}a_to_ve[i].href="https://randisafe.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/10/bv-remedy-find-most-efficient-remedies.html"}a_to_ve[i].href="https://randisafe.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
